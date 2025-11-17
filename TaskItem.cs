99dd5c440d494f786ce23cba026987b54029623a
PufferSquid <maxtjmillerx@gmail.com>
2025-11-17 11:01:48 +0000

    Created TaskItem

---
+++ TaskItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentTaskTracker
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; private set; }
+
+        public TaskItem(int id, string title, string description)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            IsCompleted = false; // false by default
+        }
+
+
+        public void MarkComplete()
+        {
+            IsCompleted = true;
+        }
+
+        public override string ToString()
+        {
+            string status = IsCompleted ? "[Complete]" : "[Incomplete]";
+            return $"{status} {Title} - {Description}";
+        }
+    }
+}
